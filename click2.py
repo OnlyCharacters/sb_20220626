216de835cf2621d33d8ca9177e941567ffdc803d
OnlyCharacters <yuxian9630@gmail.com>
2022-07-01 16:20:00 +0000

    set download path to null devices

--- click2.py
+++ click2.py
@@ -5,19 +5,21 @@ import time
 opt = webdriver.ChromeOptions()
 caps = DesiredCapabilities.CHROME
 caps['goog:loggingPrefs'] = {'performance': 'ALL'}
+prefs = {"download.default_directory": r"/dev/null"}
 opt.add_experimental_option('perfLoggingPrefs', {'enableNetwork': True})
+opt.add_experimental_option('prefs', prefs)
 driver = webdriver.Chrome(executable_path="./chromedriver", options=opt, desired_capabilities=caps)
 
-# 如果地址变了，访问 an27.cc 等等域名获取，这个盗号狗不知道买了多少域名
+# 如果地址变了，访问 an27.cc an25.cc 等等域名获取，这个盗号狗不知道买了多少域名
 driver.get("https://xjj0620.pages.dev/?lineCode=aq1")
 time.sleep(2)
 
 # 修改上面域名后，需要找到下载的位置，Chrome按F12，定位到下载位置，右键copy xpath
 driver.find_element(by="xpath", value='//*[@id="downloadand"]/button/img').click()
-time.sleep(5)
+time.sleep(1)
 
 browser_log = driver.get_log('performance')
-driver.close()
+driver.quit()
 
 log_str = ""
 for obj in browser_log:
